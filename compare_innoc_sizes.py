1f051d9943c13c36ae4066af6daa99f30c2b0dd6
eshanking <eshanking@wustl.edu>
2020-07-27 10:31:18 -0500

    Removed old files

--- compare_innoc_sizes.py
+++
@@ -1,5 +0,0 @@
-# compare simulation results of different innoculation sizes
-import numpy as np
-#import matplotlib.pyplot as plp
-import abm_variable_fitness as sim
-import matplotlib.pyplot as plt
